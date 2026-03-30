572aea7de85c989d16b9c69b4e103cace1a54562
Ilkka Wahala <ilkka.wahala@tuni.fi>
2026-03-30 13:39:21 +0300

    feat: add MyList interface

---
+++ src/main/java/fi/tuni/tamk/tiko/wahalailkka/datastructure/MyList.java
@@ -0,0 +1,62 @@
+package fi.tuni.tamk.tiko.wahalailkka.datastructure;
+
+/**
+ * A simple generic list interface.
+ *
+ * @param <T> the type of elements in this list
+ */
+public interface MyList<T> {
+    /**
+     * Adds an element to the end of the list.
+     *
+     * @param element the element to be added
+     */
+    void add(T element);
+
+    /**
+     * Returns the element at the specified index.
+     *
+     * @param index the index of the element to return
+     * @return the element at the given index
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
+    T get(int index);
+
+    /**
+     * Replaces the element at the specified index with the given element.
+     *
+     * @param index the index of the element to replace
+     * @param element the new element
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
+    void set(int index, T element);
+
+    /**
+     * Removes the element at the specified index.
+     *
+     * @param index the index of the element to remove
+     * @return the removed element
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
+    T remove(int index);
+
+    /**
+     * Removes the first occurrence of the specified element from the list.
+     *
+     * @param element the element to remove
+     * @return true if the element was found and removed, otherwise false
+     */
+    boolean remove(T element);
+
+    /**
+     * Removes all elements from the list.
+     */
+    void clear();
+
+    /**
+     * Returns the number of elements in the list.
+     *
+     * @return the size of the list
+     */
+    int size();
+}
