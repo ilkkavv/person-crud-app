f359160ab32e61e165c1c744f5fe241723bbd9ba
Ilkka Wahala <ilkka.wahala@tuni.fi>
2026-04-01 13:15:38 +0300

    feat: implement MemPersonRepository

---
+++ src/main/java/fi/tuni/tamk/tiko/wahalailkka/repository/MemPersonRepository.java
@@ -0,0 +1,119 @@
+package fi.tuni.tamk.tiko.wahalailkka.repository;
+
+import fi.tuni.tamk.tiko.wahalailkka.datastructure.MyArrayList;
+import fi.tuni.tamk.tiko.wahalailkka.datastructure.MyList;
+import fi.tuni.tamk.tiko.wahalailkka.model.Person;
+
+import java.util.Optional;
+
+/**
+ * In-memory implementation of {@link PersonRepository}.
+ * <p>
+ * This repository stores {@link Person} objects in a custom list
+ * implementation. By default, it uses {@link MyArrayList}, but the underlying
+ * data structure can be easily replaced with other list implementation as long
+ * as it mplements the {@link MyList} interface.
+ * <p>
+ * Data is not persisted and will be lost when the application terminates.
+ */
+public class MemPersonRepository implements PersonRepository {
+    /** Internal list storing persons. */
+    private final MyList<Person> personList = new MyArrayList<>();
+    /** Counter used to generate unique IDs for persons. */
+    private int nextId = 1;
+
+    /**
+     * Creates a new person and assigns an unique ID.
+     *
+     * @param person the person to create
+     * @return the created person
+     * @throws IllegalArgumentException if person is null
+     */
+    @Override
+    public Person create(final Person person) {
+        if (person == null) {
+            throw new IllegalArgumentException("Person must not be null.");
+        }
+        Person newPerson = new Person(nextId, person.firstName(),
+                person.lastName(), person.age());
+        personList.add(newPerson);
+        nextId++;
+        return newPerson;
+    }
+
+
+    /**
+     * Returns all persons stored in the repository.
+     *
+     * @return list of all persons
+     */
+    @Override
+    public MyList<Person> findAll() {
+        return personList;
+    }
+
+    /**
+     * Finds a person by ID.
+     *
+     * @param id the ID of the person
+     * @return an {@link Optional} containing the found person,
+     *         or empty if not found
+     */
+    @Override
+    public Optional<Person> findById(final int id) {
+        for (int i = 0; i < personList.size(); i++) {
+            Person listedPerson = personList.get(i);
+            if (listedPerson.id() == id) {
+                return Optional.of(listedPerson);
+            }
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Updates a person identified by ID.
+     * <p>
+     * The ID remains unchanged, but other fields are updated.
+     *
+     * @param id the ID of the person to update
+     * @param person the new person data
+     * @return an {@link Optional} containing the updated person,
+     *         or empty if no person with the given ID exists
+     * @throws IllegalArgumentException if person is null
+     */
+    @Override
+    public Optional<Person> updateById(final int id, final Person person) {
+        if (person == null) {
+            throw new IllegalArgumentException("Person must not be null.");
+        }
+        for (int i = 0; i < personList.size(); i++) {
+            Person listedPerson = personList.get(i);
+            if (listedPerson.id() == id) {
+                Person updatedPerson = new Person(listedPerson.id(),
+                        person.firstName(), person.lastName(), person.age());
+                personList.set(i, updatedPerson);
+                return Optional.of(updatedPerson);
+            }
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Deletes a person by ID.
+     *
+     * @param id the ID of the person to delete
+     * @return an {@link Optional} containing the removed person,
+     *         or empty if not found
+     */
+    @Override
+    public Optional<Person> deleteById(final int id) {
+        for (int i = 0; i < personList.size(); i++) {
+            Person person = personList.get(i);
+            if (person.id() == id) {
+                personList.remove(i);
+                return Optional.of(person);
+            }
+        }
+        return Optional.empty();
+    }
+}
