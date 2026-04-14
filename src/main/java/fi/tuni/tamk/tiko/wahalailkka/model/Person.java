007351f97cfd2493b990d1225798b7bb301c2518
Ilkka Wahala <ilkka.wahala@tuni.fi>
2026-04-14 14:51:32 +0300

    docs(model) add Javadoc comment to Person record

--- src/main/java/fi/tuni/tamk/tiko/wahalailkka/model/Person.java
+++ src/main/java/fi/tuni/tamk/tiko/wahalailkka/model/Person.java
@@ -1,3 +1,11 @@
 package fi.tuni.tamk.tiko.wahalailkka.model;
 
+/**
+ * Represents a person entity stored in the system.
+ *
+ * @param id the unique identifier of the person
+ * @param firstName the first name of the person
+ * @param lastName the last name of the person
+ * @param age the age of the person
+ */
 public record Person(int id, String firstName, String lastName, int age) { }
