c72e76797ff913064989a3194809c5035df11355
Ilkka Wahala <ilkka.wahala@tuni.fi>
2026-04-14 14:51:58 +0300

    docs(model) add Javadoc comment to PersonData record

--- src/main/java/fi/tuni/tamk/tiko/wahalailkka/model/PersonData.java
+++ src/main/java/fi/tuni/tamk/tiko/wahalailkka/model/PersonData.java
@@ -1,3 +1,12 @@
 package fi.tuni.tamk.tiko.wahalailkka.model;
 
+/**
+ * Represents input data for creating or updating a {@link Person}.
+ * <p>
+ * This record does not include an ID, as it is assigned by the repository.
+ *
+ * @param firstName the first name of the person
+ * @param lastName the last name of the person
+ * @param age the age of the person
+ */
 public record PersonData(String firstName, String lastName, int age) { }
