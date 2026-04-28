5af4d256201efcf3dce736c98ff5f4655edddf62
Ilkka Wahala <ilkka.wahala@tuni.fi>
2026-04-28 14:15:37 +0300

    feat(controller): add PersonListResult for search operations

---
+++ src/main/java/fi/tuni/tamk/tiko/wahalailkka/controller/PersonListResult.java
@@ -0,0 +1,41 @@
+package fi.tuni.tamk.tiko.wahalailkka.controller;
+
+import fi.tuni.tamk.tiko.wahalailkka.datastructure.MyArrayList;
+import fi.tuni.tamk.tiko.wahalailkka.datastructure.MyList;
+import fi.tuni.tamk.tiko.wahalailkka.model.Person;
+
+/**
+ * Represents the result of an operation that returns a list of persons.
+ * <p>
+ * This result is used for operations such as search, where the operation may
+ * either succeed with a list of persons or fail due to validation errors.
+ *
+ * @param isSuccess true if the operation succeeded, false otherwise
+ * @param personList the resulting list of persons
+ * @param validationErrors validation error messages, empty if none
+ */
+public record PersonListResult(boolean isSuccess, MyList<Person> personList,
+                               MyList<String> validationErrors) {
+    /**
+     * Creates a successful result containing the given list of persons.
+     *
+     * @param personList the resulting list of persons
+     * @return a successful result containing the person list
+     */
+    public static PersonListResult success(final MyList<Person> personList) {
+        return new PersonListResult(true, personList,
+                new MyArrayList<String>());
+    }
+
+    /**
+     * Creates a failed result containing validation errors.
+     *
+     * @param validationErrors the validation error messages
+     * @return a failed result containing validation errors
+     */
+    public static PersonListResult failure(
+            final MyList<String> validationErrors) {
+        return new PersonListResult(false, new MyArrayList<Person>(),
+                validationErrors);
+    }
+}
