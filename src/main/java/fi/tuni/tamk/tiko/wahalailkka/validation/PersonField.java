6f9f4950e07574afb83101e37ce20ec5bfc5c086
Ilkka Wahala <ilkka.wahala@tuni.fi>
2026-05-08 13:47:54 +0300

    feat(validation): add ID to PersonField enum

--- src/main/java/fi/tuni/tamk/tiko/wahalailkka/validation/PersonField.java
+++ src/main/java/fi/tuni/tamk/tiko/wahalailkka/validation/PersonField.java
@@ -4,6 +4,7 @@ package fi.tuni.tamk.tiko.wahalailkka.validation;
  * Defines the available person form fields used in validation.
  */
 public enum PersonField {
+    ID,
     FIRST_NAME,
     LAST_NAME,
     AGE
