b786500194a840ec643d69a37139c2cdcd87b8fe
Ilkka Wahala <ilkka.wahala@tuni.fi>
2026-05-08 16:02:01 +0300

    feat(validation): add common message accessor to ValidationError

--- src/main/java/fi/tuni/tamk/tiko/wahalailkka/validation/ValidationError.java
+++ src/main/java/fi/tuni/tamk/tiko/wahalailkka/validation/ValidationError.java
@@ -1,4 +1,15 @@
 package fi.tuni.tamk.tiko.wahalailkka.validation;
 
+/**
+ * Represents a generic validation error.
+ * <p>
+ * Implementations provide an error message describing why validation failed.
+ */
 public interface ValidationError {
+    /**
+     * Returns the validation error message.
+     *
+     * @return the validation error message
+     */
+    String getMessage();
 }
