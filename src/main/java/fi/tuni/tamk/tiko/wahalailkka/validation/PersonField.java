7ac223a83e84750960aed6ab5fc0d6f6ad65f632
Ilkka Wahala <ilkka.wahala@tuni.fi>
2026-05-08 13:18:05 +0300

    feat(validation): add PersonField enum for typed validation fields

---
+++ src/main/java/fi/tuni/tamk/tiko/wahalailkka/validation/PersonField.java
@@ -0,0 +1,10 @@
+package fi.tuni.tamk.tiko.wahalailkka.validation;
+
+/**
+ * Defines the available person form fields used in validation.
+ */
+public enum PersonField {
+    FIRST_NAME,
+    LAST_NAME,
+    AGE
+}
