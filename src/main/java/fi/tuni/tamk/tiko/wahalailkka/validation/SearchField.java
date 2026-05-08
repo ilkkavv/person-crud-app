abd1b6b8bf41cb6501e4783c85a8deacfe78a544
Ilkka Wahala <ilkka.wahala@tuni.fi>
2026-05-08 14:04:29 +0300

    refactor(validation): rename UserInput enum to SearchField

--- src/main/java/fi/tuni/tamk/tiko/wahalailkka/validation/SearchField.java
+++ src/main/java/fi/tuni/tamk/tiko/wahalailkka/validation/SearchField.java
@@ -3,7 +3,7 @@ package fi.tuni.tamk.tiko.wahalailkka.validation;
 /**
  * Defines input fields used in user input validation.
  */
-public enum UserInput {
+public enum SearchField {
     SEARCH,
     MIN_AGE,
     MAX_AGE
