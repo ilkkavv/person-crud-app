994801956daeebcca9bdc9b7a817abb9281bd43f
Ilkka Wahala <ilkka.wahala@tuni.fi>
2026-05-08 14:01:38 +0300

    feat(validation): add UserInput enum for search validation fields

---
+++ src/main/java/fi/tuni/tamk/tiko/wahalailkka/validation/UserInput.java
@@ -0,0 +1,10 @@
+package fi.tuni.tamk.tiko.wahalailkka.validation;
+
+/**
+ * Defines input fields used in user input validation.
+ */
+public enum UserInput {
+    SEARCH,
+    MIN_AGE,
+    MAX_AGE
+}
