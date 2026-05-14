d2112fd797edd912d4c1163ea1594ad37ec24c28
Ilkka Wahala <ilkka.wahala@tuni.fi>
2026-05-14 14:23:50 +0300

    docs(validation): add Javadoc to PersonField enum

--- src/main/java/fi/tuni/tamk/tiko/wahalailkka/validation/PersonField.java
+++ src/main/java/fi/tuni/tamk/tiko/wahalailkka/validation/PersonField.java
@@ -4,8 +4,12 @@ package fi.tuni.tamk.tiko.wahalailkka.validation;
  * Defines the available person form fields used in validation.
  */
 public enum PersonField {
+    /** Person identifier field. */
     ID,
+    /** Person first name field. */
     FIRST_NAME,
+    /** Person last name field. */
     LAST_NAME,
+    /** Person age field. */
     AGE
 }
