cf88c5381874affba07a566dfcba083a7968be3c
Ilkka Wahala <ilkka.wahala@tuni.fi>
2026-05-14 14:21:40 +0300

    docs(ui): add Javadoc to AppUi interface

--- src/main/java/fi/tuni/tamk/tiko/wahalailkka/ui/AppUi.java
+++ src/main/java/fi/tuni/tamk/tiko/wahalailkka/ui/AppUi.java
@@ -1,6 +1,16 @@
 package fi.tuni.tamk.tiko.wahalailkka.ui;
 
+/**
+ * Common interface for application user interfaces.
+ * <p>
+ * Implementations are responsible for starting and running the
+ * application user interface, such as a command-line interface
+ * or graphical user interface.
+ */
 @FunctionalInterface
 public interface AppUi {
+    /**
+     * Starts and runs the user interface.
+     */
     void run();
 }
