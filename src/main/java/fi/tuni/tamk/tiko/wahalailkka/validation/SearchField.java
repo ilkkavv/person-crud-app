73416c4a19639c80d2cc5fe76f0c846fdc856915
Ilkka Wahala <ilkka.wahala@tuni.fi>
2026-05-14 14:25:04 +0300

    docs(validation): add Javadoc to SearchField enum

--- src/main/java/fi/tuni/tamk/tiko/wahalailkka/validation/SearchField.java
+++ src/main/java/fi/tuni/tamk/tiko/wahalailkka/validation/SearchField.java
@@ -4,7 +4,10 @@ package fi.tuni.tamk.tiko.wahalailkka.validation;
  * Defines input fields used in user input validation.
  */
 public enum SearchField {
+    /** General text search field. */
     SEARCH,
+    /** Minimum age search field. */
     MIN_AGE,
+    /** Maximum age search field. */
     MAX_AGE
 }
