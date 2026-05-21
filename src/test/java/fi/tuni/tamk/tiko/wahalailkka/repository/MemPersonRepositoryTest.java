2be6e073af2de1632609cae9ae9ce01454e2e346
Ilkka Wahala <ilkka.wahala@tuni.fi>
2026-05-21 11:37:45 +0300

    test(repository): verify create returns correct person data

--- src/test/java/fi/tuni/tamk/tiko/wahalailkka/repository/MemPersonRepositoryTest.java
+++ src/test/java/fi/tuni/tamk/tiko/wahalailkka/repository/MemPersonRepositoryTest.java
@@ -1,7 +1,10 @@
 package fi.tuni.tamk.tiko.wahalailkka.repository;
 
+import fi.tuni.tamk.tiko.wahalailkka.model.Person;
+import fi.tuni.tamk.tiko.wahalailkka.model.PersonData;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MemPersonRepositoryTest {
@@ -10,5 +13,13 @@ public class MemPersonRepositoryTest {
         MemPersonRepository memRepo = new MemPersonRepository();
 
         assertTrue(memRepo.findAll().isEmpty());
+    }
+
+    @Test
+    void createReturnCorrectData() {
+        MemPersonRepository memRepo = new MemPersonRepository();
+
+        assertEquals(new Person(1, "John", "Doe", 35),
+                     memRepo.create(new PersonData("John", "Doe", 35)));
     }
 }
