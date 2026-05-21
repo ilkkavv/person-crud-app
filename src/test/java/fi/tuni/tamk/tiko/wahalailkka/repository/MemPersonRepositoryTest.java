8163e0e70f2c7a1d80adf3557014684a820d19d7
Ilkka Wahala <ilkka.wahala@tuni.fi>
2026-05-21 11:48:51 +0300

    refactor(test): extract reusable test data constants

--- src/test/java/fi/tuni/tamk/tiko/wahalailkka/repository/MemPersonRepositoryTest.java
+++ src/test/java/fi/tuni/tamk/tiko/wahalailkka/repository/MemPersonRepositoryTest.java
@@ -8,6 +8,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MemPersonRepositoryTest {
+    private static final PersonData JOHN_DATA = new PersonData(
+            "John", "Doe", 35);
+    private static final PersonData JANE_DATA = new PersonData(
+            "Jane", "Doe", 34);
+    private  static final PersonData JACK_DATA = new PersonData(
+            "Jack", "Doe", 41);
+
     @Test
     void findAllReturnEmpty() {
         MemPersonRepository memRepo = new MemPersonRepository();
@@ -20,18 +27,15 @@ public class MemPersonRepositoryTest {
         MemPersonRepository memRepo = new MemPersonRepository();
 
         assertEquals(new Person(1, "John", "Doe", 35),
-                     memRepo.create(new PersonData("John", "Doe", 35)));
+                     memRepo.create(JOHN_DATA));
     }
 
     @Test
     void createShouldIncrementId() {
         MemPersonRepository memRepo = new MemPersonRepository();
 
-        assertEquals(1, memRepo.create(new PersonData(
-                "John", "Doe", 35)).id());
-        assertEquals(2, memRepo.create(new PersonData(
-                "Jane", "Doe", 32)).id());
-        assertEquals(3, memRepo.create(new PersonData(
-                "Jack", "Doe", 41)).id());
+        assertEquals(1, memRepo.create(JOHN_DATA).id());
+        assertEquals(2, memRepo.create(JANE_DATA).id());
+        assertEquals(3, memRepo.create(JACK_DATA).id());
     }
 }
